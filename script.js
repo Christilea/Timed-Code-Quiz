0d55d90f464fc8ecb1a566fe844e04d5da16cff3
Christi <mahomes4president@gmail.com>
2021-07-12 21:11:14 -0500

    fixing files

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 //run this on page start
-var startPageEl = document.querySelector("#Cover");
+var startPg = document.querySelector("#Cover");
 var startBtnEl = document.querySelector("#startBtn")
 var sbLinkEl = document.querySelector("#sbLink");
-var scoreboardEl = document.querySelector("#score-card");
-var question1El = document.querySelector("#Question1");
-var question2El = document.querySelector("#Question2");
-var question3El = document.querySelector("#Question3");
-var question5El = document.querySelector("#Question4");
-var question5El = document.querySelector("#Question5");
-var inputScoreEl = document.querySelector("#inputScoreInfo");
+var scorebrd = document.querySelector("#score-card");
+var quest1 = document.querySelector("#Question1");
+var quest2 = document.querySelector("#Question2");
+var quest3 = document.querySelector("#Question3");
+var quest4 = document.querySelector("#Question4");
+var quest5 = document.querySelector("#Question5");
+var inputScore = document.querySelector("#inputScoreInfo");
 var questionArr = ["Question1", "Question2", "Question3", "Question4", "Question5"];
 var answerBtn = document.querySelectorAll("button[correct]");
-var correctFooterEl = document.querySelector("#correctFooter");
+var correctFooter = document.querySelector("#correctFooter");
 var incorrectFooterEl = document.querySelector("#incorrectFooter");
 var scoreboardList = document.querySelector("#scoreboard");
-var sbTryAgainBtn = document.querySelector("#tryAgain");
+var tryAgainBtn = document.querySelector("#tryAgain");
 var sbClearBtn = document.querySelector("#clearBtn");
 
-startPageEl.className = startPageEl.className.replace("hidden", "");
+startPg.className = startPg.className.replace("hidden", "");
 
-//sets View High Scores link --> on click to go to Scoreboard
 sbLinkEl.addEventListener("click", function(){
-  showCard(scoreboardEl);
+  showCard(scorebrd);
 });
 
-//start btn to show first question
+// shows first question
 startBtnEl.addEventListener("click", function(){
   questionArr = ["Question1", "Question2", "Question3", "Question4", "Question5"];
-  showRandomQuestion(questionArr, inputScoreEl);
+  showRandQuest(questionArr, inputScore);
   setTime(timerEl);
 });
 
-//loop through each answer button and add click event
+//loop through each answer button 
 for(i=0; i<answerBtn.length; i++){
   var Btn = answerBtn.item(i);
   Btn.addEventListener("click", function(e){
@@ -39,7 +38,7 @@ for(i=0; i<answerBtn.length; i++){
     var btnEl = e.target||e.srcElement;
     var isCorrectReturn = rightAnswer(btnEl);
     if (isCorrectReturn == "true"){
-      showRandomQuestion(questionArr, inputScoreEl);
+      showRandQuest(questionArr, inputScore);
     }
     else{
       secondsLeft = secondsLeft - 10;
@@ -64,7 +63,7 @@ function setTime(timerEl) {
     //if time runs out (timer = or < 0), show input card & score = 0
     if(secondsLeft <= 0) {
       clearInterval(timerInterval);
-      showCard(inputScoreEl);
+      showCard(inputScore);
       secondsLeft = 0;
       finalTime.textContent = secondsLeft;
     }
@@ -72,82 +71,70 @@ function setTime(timerEl) {
   }, 1000);
 }
 
-var initalsEl = document.getElementById("initalsInput");
-var initialsSubmitBtn = document.getElementById("initalsSubmit");
+var initalsAdd = document.getElementById("initalsInput");
+var initialsSubBtn = document.getElementById("initalsSubmit");
 
-initialsSubmitBtn.addEventListener("click", function(){
-  var initalsSubmitted = initalsEl.value;
-  var userInitialsArr = [initalsSubmitted, secondsLeft];
-  initalsEl.value = "";
-  // example: sbItemsArr[0] = ABC, sbItemsArr[1] = 25;
+initialsSubBtn.addEventListener("click", function(){
+  var initalsSubmit = initalsAdd.value;
+  var userInitialsArr = [initalsSubmit, secondsLeft];
+  initalsAdd.value = "";
+  
 
-  var highScoresArr = JSON.parse(localStorage.getItem("highScoresArr"));
-  //check if scores exist, if none exist, push score into array
-  if (highScoresArr == null){
+  var highScores = JSON.parse(localStorage.getItem("highScores"));
+  //if no score exist, push score into array
+  if (highScores == null){
     localStorage.setItem("highScoresArr", JSON.stringify(userInitialsArr));
   }
-    //push HS into array in order starting with highest score
+    //push high score into array 
     else{
-      for (i = 1; i < highScoresArr.length; i+=2) {
-        currentHS = highScoresArr[i];
+      for (i = 1; i < highScores.length; i+=2) {
+        currentHS = highScores[i];
         
         var isHighscore = false;
-        if(userInitialsArr[1] > highScoresArr[i]){
-          highScoresArr.splice(i-1, 0, userInitialsArr[1]);
-          highScoresArr.splice(i-1, 0, userInitialsArr[0]);
+        if(userInitialsArr[1] > highScores[i]){
+          highScores.splice(i-1, 0, userInitialsArr[1]);
+          highScores.splice(i-1, 0, userInitialsArr[0]);
           isHighscore = true;
           break;
         }
       }
       //if we didn't insert our scores anywhere and if there is space in the array it must be the low score.
-      if (isHighscore == false && highScoresArr.length < 10){
-        highScoresArr.push(userInitialsArr[0]);
-        highScoresArr.push(userInitialsArr[1]);
+      if (isHighscore == false && highScores2.length < 10){
+        highScores.push(userInitialsArr[0]);
+        highScores.push(userInitialsArr[1]);
       }
-      localStorage.setItem("highScoresArr", JSON.stringify(highScoresArr));
+      localStorage.setItem("highScores", JSON.stringify(highScores2));
     }
-  showCard(scoreboardEl);
+  showCard(scorebrd);
 });
 
 // try again button
-sbTryAgainBtn.addEventListener("click", function(){
+tryAgainBtn.addEventListener("click", function(){
   showCard(startPageEl);
   secondsLeft=60;
   timerEl.textContent = secondsLeft;
 });
 
-// page clear button
 sbClearBtn.addEventListener("click", function(){
-  //clear scoreboard out of DOM
   while (scoreboardList.children.length > 0) {
     scoreboardList.children.item(0).remove();
   }
-  //clear scoreboard out of localStorage
-  localStorage.removeItem("highScoresArr");
+  localStorage.removeItem("highScores");
 });
 
 
-//end page start
-
-
-
-//randomly choose question from array
-function showRandomQuestion(questionArr, inputScoreEl){ 
+function showRandQuest(questionArr, inputScore){ 
   //checks length of question array
   if (questionArr.length == 0){
-    showCard(inputScoreEl);
+    showCard(inputScore);
     clearInterval(timerInterval);
     finalTime.textContent = secondsLeft;
 
   }
   else{ 
-    //chooses random index from questions array
     var i = Math.floor(Math.random() * questionArr.length);
-    //get element of question array id
     var chosenQ = document.getElementById(questionArr[i]);
-    //remove randomly chosen question from array
     questionArr.splice(i, 1);
-    //shows randomly chosen qustion
     showCard(chosenQ);
   }
 }
@@ -156,32 +143,30 @@ function showCard(calledCard){
   var allCards = document.querySelectorAll(".container");
   //loops through all cards
   for(i=0; i<allCards.length; i++){
-    //get current card and set to var
     var currentCard = allCards.item(i);
-    //if the current card's class list does not include the hidden class, then add the hidden class
     if(currentCard.className.includes("hidden") == false){
       currentCard.className += ' hidden';
     }   
   }
-  //show calledCard
+  //show card
   calledCard.className = calledCard.className.replace("hidden", "");
   console.log(calledCard.className);
 
   //check is card shown is the scoreboard card
-  if (calledCard == scoreboardEl){
+  if (calledCard == scorebrd){
     // clear scoreboard
     while (scoreboardList.children.length > 0) {
       scoreboardList.children.item(0).remove();
     }
-    var highScoresArr = JSON.parse(localStorage.getItem("highScoresArr"));
+    var highScores = JSON.parse(localStorage.getItem("highScores"));
 
-    // Render scoreboard scores as li for each [initals, score]
-    if (highScoresArr != null){
-      for (var i = 0; i < highScoresArr.length; i+=2) {
-        var currentHSli = highScoresArr[i] + ": " + highScoresArr[i+1];
+    // Render scoreboard scores 
+    if (highScores != null){
+      for (var i = 0; i < highScores.length; i+=2) {
+        var currentHS = highScores[i] + ": " + highScores2[i+1];
 
         var li = document.createElement("li");
-        li.textContent = currentHSli;
+        li.textContent = currentHS;
 
         scoreboardList.appendChild(li);
       }
@@ -193,14 +178,13 @@ function showCard(calledCard){
 function rightAnswer(btnEl){
   var isCorrect = btnEl.getAttribute("correct");
   if (isCorrect == "true"){
-    // show correct footer & hide incorrect footer
-    correctFooterEl.className = correctFooterEl.className.replace("hidden", "");
+    correctFooter.className = correctFooter.className.replace("hidden", "");
     setTimeout(function(){
-      correctFooterEl.className += ' hidden';
+      correctFooter.className += ' hidden';
     }, 500);
   }
   else{
-    // show incorrect footer & hide correct footer
+    // incorrect footer & correct footer
     incorrectFooterEl.className = incorrectFooterEl.className.replace("hidden", "");
     setTimeout(function(){
       incorrectFooterEl.className += ' hidden';
